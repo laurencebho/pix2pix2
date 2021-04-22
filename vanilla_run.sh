ebc0abdede2346b6f733e9c6b1144463e49820d4
Laurie <laurence.ho@durham.ac.uk>
2021-04-22 02:20:37 +0100

    run script

---
+++ vanilla_run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH --gres=gpu
+#SBATCH -p ug-gpu-small
+#SBATCH --qos=short
+#SBATCH -t 01-00:00:00
+#SBATCH --job-name=pix2pixLaurie
+#SBATCH --mem=22G
+#SBATCH --mail-user laurence.ho@durham.ac.uk
+#SBATCH --mail-type=ALL
+
+source ~/pix2pixenv/bin/activate
+module load cuda/10.1-cudnn7.6
+
+
+python train.py --dataroot ./datasets/vanilla_pix2pix_files --name vanilla_pix2pix_20_5 --model pix2pix --direction BtoA --display_id=0 --input_nc 1 --output_nc 3
+
+python test.py --dataroot ./datasets/vanilla_pix2pix_files --name vanilla_pix2pix_20_5 --model pix2pix --direction BtoA --input_nc 1 --output_nc 3
